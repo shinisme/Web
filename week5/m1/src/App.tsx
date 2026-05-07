c72edbbecbdfa59d9793baae6b8e80f171025581
shinisme <syshin0608@gmail.com>
2026-05-07 15:49:51 +0900

    feat: 승구리 week5-m1

---
+++ week5/m1/src/App.tsx
@@ -0,0 +1,51 @@
+import './App.css'
+import { createBrowserRouter, RouterProvider, type RouteObject } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFoundErr from './pages/NotFoundErr'
+import Login from './pages/Login'
+import HomeLayout from './layouts/HomeLayout'
+import SignUp from './pages/SignUp'
+import MyPage from './pages/Mypage'
+import { AuthProvider } from './context/AuthContext'
+import PrivateLayout from './layouts/PrivateLayout'
+
+const publicRoutes:RouteObject[] = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundErr />,
+    children: [
+      {index: true, element: <Home />},
+      {path: 'login', element: <Login />},
+      {path: 'signup', element: <SignUp />},
+    ]
+  },
+];
+
+const privateRoutes:RouteObject[] = [
+  {
+    path: "/",
+    element: <PrivateLayout />,
+    errorElement: <NotFoundErr />,
+    children: [
+      {path: 'my', element: <MyPage />},
+    ],
+  },
+];
+
+const router = createBrowserRouter([
+  ...publicRoutes,
+  ...privateRoutes,
+])
+
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <RouterProvider router={router}/>
+      </AuthProvider>
+    </>
+  )
+}
+
+export default App
